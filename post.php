de9a4051fcb5181ac158f9f636261ab025a82009
root <benjholla@gmail.com>
2015-02-22 21:05:09 -0800

    added title and favicon

--- post.php
+++ post.php
@@ -10,9 +10,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="/favicon.png">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
-    <title>Completely Digital Clips</title>
+    <title>Completely Digital Clips - Post Video</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/static/css/bootstrap.css" rel="stylesheet">
