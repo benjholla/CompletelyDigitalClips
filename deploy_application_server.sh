8c19c94ed7168bf54cce7776af41fc2f70bee62d
root <root@cdc.com>
2015-03-22 09:03:56 -0700

    tweaks to deploy script

--- deploy_application_server.sh
+++ deploy_application_server.sh
@@ -41,8 +41,8 @@ rm Application/config.php
 mv config_template Application/config.php
 
 # copy and replace the file contents of the application source to the webserver directory
-rm -rf /var/www/html/*
-cp -a Application/. /var/www/html/
+sudo rm -rf /var/www/html/*
+sudo cp -a Application/. /var/www/html/
 
 # all done
-echo "Finished."
+printf "\nFinished.\n"
