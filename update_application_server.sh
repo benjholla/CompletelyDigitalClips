ddffc9f12e75aa4355f1d8609030bb3f0b55825f
root <root@cdc.com>
2015-03-23 14:14:36 -0700

    added some cleaner update uptions to not overwrite the config file

--- update_application_server.sh
+++ update_application_server.sh
@@ -5,14 +5,15 @@ clear
 
 # reset any changes in the git repo to head of master and remove untracked files and pull updates from master repo
 echo "Resetting source repository..."
-git reset --hard
-git clean -x -f
-git pull https://github.com/benjholla/CompletelyDigitalClips.git master &> /dev/null
-git pull origin master
+#git reset --hard
+#git clean -x -f
+#git pull https://github.com/benjholla/CompletelyDigitalClips.git master &> /dev/null
+#git pull origin master
 
 # copy and replace the file contents of the application source to the webserver directory
 # don't replace config.php file
-sudo rm -rf /var/www/!(config.php)
+find /var/www/ -type f -not -name 'config.php' -delete
+find /var/www/ -type d -empty -delete
 sudo cp -a -n Application/. /var/www/
 
 # all done
