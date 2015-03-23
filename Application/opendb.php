7ad603c39019be2f37fe9cb70d15c7eec467632b
root <root@cdc.com>
2015-03-23 14:28:23 -0700

    forgot to change variable name

--- Application/opendb.php
+++ Application/opendb.php
@@ -1,6 +1,6 @@
 <?php
 // opens the database connection
-$conn = mysql_connect($DATABASE_HOSTNAME, $DATABASE_USERNAME, $DATABASE_PASSWORD) or die ('Error connecting to mysql');
+$conn = mysql_connect($DATABASE_IP, $DATABASE_USERNAME, $DATABASE_PASSWORD) or die ('Error connecting to mysql');
 
 // connects to the specific database on the MySQL server
 mysql_select_db($DATABASE_NAME);
