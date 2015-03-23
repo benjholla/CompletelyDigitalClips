7dcd3d1a80a471d2baa942c3faa8908ce5bb18e7
Ben Holland <benjholla@gmail.com>
2015-03-23 15:53:22 -0500

    Create create_application_database.sh

---
+++ create_application_database.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MYSQL=`which mysql`
+DATABASE_NAME="application"
+DATABASE_USERNAME="root"
+DATABASE_PASSWORD="cdc"
+
+Q1="CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
+Q2="GRANT USAGE ON *.* TO \`$DATABASE_USERNAME\`@'0.0.0.0' IDENTIFIED BY '$DATABASE_PASSWORD' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;"
+Q3="GRANT ALL PRIVILEGES ON \`$DATABASE_NAME\` . * TO '$DATABASE_USERNAME'@'0.0.0.0';"
+Q4=`cat Application/create-users.sql`
+Q5=`cat Application/create-clips.sql`
+
+SQL="${Q1}${Q2}${Q3}${Q4}${Q5}"
+
+clear
+
+$MYSQL -uroot -p -e "$SQL"
+
+echo "Finished."
