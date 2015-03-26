7e3cb2f12f8cf106c1e2294d1558294a6349d9e6
root <root@cdc.com>
2015-03-26 00:53:50 -0700

    fixing update script

--- update_application_server.sh
+++ update_application_server.sh
@@ -5,10 +5,10 @@ clear
 
 # reset any changes in the git repo to head of master and remove untracked files and pull updates from master repo
 echo "Resetting source repository..."
-#git pull https://github.com/benjholla/CompletelyDigitalClips.git master &> /dev/null
-#git pull origin master
-#git reset --hard
-#git clean -x -f
+git pull https://github.com/benjholla/CompletelyDigitalClips.git master &> /dev/null
+git pull origin master
+git reset --hard
+git clean -x -f
 
 # copy and replace the file contents of the application source to the webserver directory
 # don't replace config.php file
