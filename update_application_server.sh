fcc1c55c02c6972842836ae4bdee0aa3b3211cd4
root <root@cdc.com>
2015-03-26 00:53:31 -0700

    fixing update script

--- update_application_server.sh
+++ update_application_server.sh
@@ -5,14 +5,15 @@ clear
 
 # reset any changes in the git repo to head of master and remove untracked files and pull updates from master repo
 echo "Resetting source repository..."
-git pull https://github.com/benjholla/CompletelyDigitalClips.git master &> /dev/null
-git pull origin master
-git reset --hard
-git clean -x -f
+#git pull https://github.com/benjholla/CompletelyDigitalClips.git master &> /dev/null
+#git pull origin master
+#git reset --hard
+#git clean -x -f
 
 # copy and replace the file contents of the application source to the webserver directory
 # don't replace config.php file
-sudo find /var/www/ -type f ! -name 'config.php' ! -name '*.mp4' ! -name '*.ogg' ! -name '*.webm' -delete ! -name '*.png' -delete
+sudo find /var/www/ -type f ! -name '*.mp4' ! -name '*.ogg' ! -name '*.webm' -delete
+sudo find /var/www/ -type f ! -name 'config.php' ! -name '*.png' -delete
 sudo find /var/www/ -type d -empty -delete
 sudo cp -a -n Application/. /var/www/
 
