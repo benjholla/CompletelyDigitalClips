14822c306f85465cb14e8b712772eb86a88341f3
root <root@cdc.com>
2015-03-26 08:17:08 -0700

    quickly fixing broken script from last night

--- update_application_server.sh
+++ update_application_server.sh
@@ -13,13 +13,13 @@ git clean -x -f
 # copy and replace the file contents of the application source to the webserver directory
 # don't replace config.php file
 tmpdir=`mktemp -d`
+sudo rm /var/www/media/index.html
 sudo cp -a -n /var/www/media/. $tmpdir/
 sudo cp -a -n /var/www/config.php $tmpdir/
 ls -la $tmpdir
 sudo find /var/www/ -type f -delete
 sudo find /var/www/ -type d -empty -delete
-sudo cp $tmpdir/config.php /var/www/config.php
-sudo rm $tmpdir/index.html
+sudo cp -a -n $tmpdir/config.php /var/www/config.php
 sudo rm $tmpdir/config.php
 sudo cp -a -n Application/. /var/www/
 sudo cp -a -n $tmpdir/. /var/www/media/
